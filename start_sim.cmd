af30a0ef3efd3aba1ad9850860cb2c51b4556f1d
amarkov <amarkov@internal.oculus-tech.com>
2016-12-21 13:15:45 +0400

    added hackrf commands

--- start_sim.cmd
+++ start_sim.cmd
@@ -3,3 +3,8 @@
 ./gps-sdr-sim -s 2500000 -e brdc3500.16n -T
 
 ./gps-sdr-sim -s 2500000 -e brdc3500.16n -t 2016/12/15,12:00:00
+
+#For HackRF
+#!!! Make sure that -b 8 and -s 2600000 !!!
+./gps-sdr-sim -b 8 -s 2600000 -e brdc3500.16n -t 2016/12/15,12:00:00 -l 24.495774,54.358074,1
+hackrf_transfer -t gpssim.bin -f 1575420000 -s 2600000 -a 1 -x 30
