ec9abeecb65d0e87ffeae2dfef74a5e3cc45471b
andrey <a.vmarkov@gmail.com>
2017-01-04 15:39:54 +0400

    hackrf command without amplification

--- start_sim.cmd
+++ start_sim.cmd
@@ -7,4 +7,6 @@
 #For HackRF
 #!!! Make sure that -b 8 and -s 2600000 !!!
 ./gps-sdr-sim -b 8 -s 2600000 -e brdc3500.16n -t 2016/12/15,12:00:00 -l 24.495774,54.358074,1
+
+hackrf_transfer -t gpssim.bin -f 1575420000 -s 2600000 -a 0 -x 30
 hackrf_transfer -t gpssim.bin -f 1575420000 -s 2600000 -a 1 -x 30
